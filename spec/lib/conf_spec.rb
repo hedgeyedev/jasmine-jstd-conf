c19e38c8ce38de9d415b1ba54d3b46f08578115e
Benjamin Oakes <boakes@hedgeye.com>
2012-01-27 09:07:16 -0500

    "the config file content includes the files specified in the Jasmine config"

--- spec/lib/conf_spec.rb
+++ spec/lib/conf_spec.rb
@@ -2,8 +2,25 @@ require 'spec_helper'
 
 module Jasmine::JSTD
   describe Conf do
-    it 'should have specs' do
-      1.should == 1
+    before(:each) do
+      config = mock('Jasmine::Config', :js_files => [
+        '/__spec__/FooSpec.rb',
+        '/__spec__/BarSpec.rb',
+      ])
+      Jasmine::Config.stub(:new) { config }
+    end
+
+    describe 'the config file content' do
+      def render
+        Jasmine::JSTD::Conf.render
+      end
+
+      it 'includes the files specified in the Jasmine config' do
+        rendered = render
+        
+        rendered.should match(%r{^  - spec/javascripts/FooSpec.rb})
+        rendered.should match(%r{^  - spec/javascripts/BarSpec.rb})
+      end
     end
   end
 end
