39894f0222fb20a67222f9ada8154e09df3117fa
Benjamin Oakes <boakes@hedgeye.com>
2012-01-27 08:57:18 -0500

    Passing specs

---
+++ spec/lib/conf_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+module Jasmine::JSTD
+  describe Conf do
+    it 'should have specs' do
+      1.should == 1
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'rspec'
+
+require 'jasmine-jstd-conf'
+
+RSpec.configure do |c|
+  c.mock_with :rspec
+end
